5b2801a9f754004ea0a5ab600a82991bdd0442b6
matthandi <matthandi@online.de>
2023-07-05 10:08:12 +0000

    deployment script corrected

--- pipeline/deploy.sh
+++ pipeline/deploy.sh
@@ -5,13 +5,14 @@ set -e
 echo "starting $0 {"
 
 rm -rf unicornmain_webserver/
-git clone https://github.com/matthandi/unicornmain_webserver.git
+git clone https://$MACHINE_USER_TOKEN@github.com/matthandi/unicornmain_webserver.git
 cd unicornmain_webserver/
 
 cp ../index.html .
 git add --all
-git commit -m "$(date) "
+git commit -m "$(date) " || true
 git push
+cd ../
 
 rm -rf unicornmain_webserver/
 echo "finished $0 }"
