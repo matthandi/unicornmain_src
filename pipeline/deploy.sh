599b43bb2777d9393888ed1b0bd2c773c5b7d0f9
matthandi <matthandi@online.de>
2023-07-05 10:12:45 +0000

    full pipeline

--- pipeline/deploy.sh
+++ pipeline/deploy.sh
@@ -6,7 +6,10 @@ echo "starting $0 {"
 
 rm -rf unicornmain_webserver/
 git clone https://$MACHINE_USER_TOKEN@github.com/matthandi/unicornmain_webserver.git
+
 cd unicornmain_webserver/
+git config user.email "user@machine.com"
+git config user.name  "Machine User"
 
 cp ../index.html .
 git add --all
